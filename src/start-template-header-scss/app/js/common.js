0dd4e2111a3d1c78ce3094ac80713d6cca35f902
egrechnev <egrechnev@gmail.com>
2018-01-29 04:22:39 +0400

    src

---
+++ src/start-template-header-scss/app/js/common.js
@@ -0,0 +1,14 @@
+$(function() {
+
+	// Custom JS
+
+	//Chrome Smooth Scroll
+	// try {
+	// 	$.browserSelector();
+	// 	if($("html").hasClass("chrome")) {
+	// 		$.smoothScroll();
+	// 	}
+	// } catch(err) {
+
+	// }
+});
