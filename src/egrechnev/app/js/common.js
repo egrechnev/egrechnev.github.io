37ed4a2d8be4e619b8e78195873cea4e64555c20
egrechnev <egrechnev@gmail.com>
2018-06-05 08:42:47 +0400

    update

---
+++ src/egrechnev/app/js/common.js
@@ -0,0 +1,133 @@
+$(function() {
+
+//----Portfolio |- start ->
+//
+//--Sort - https://www.kunkalabs.com/mixitup/
+var mixer = mixitup('.portfolio_grid');
+
+//--Add class="active"
+$(".s_portfolio li").click(function(){
+	$(".s_portfolio li").removeClass("active");
+	$(this).addClass("active");
+});
+
+//--Popup descr
+$('.popup_content, .game').magnificPopup({
+  type:'inline',
+  midClick: true,
+
+//--Add class 'mfp-fade' for <--Animation-->
+  removalDelay: 300,
+//--Class that is added to popup wrapper and background
+  mainClass: 'mfp-fade'
+});
+
+//ID--for link and descr
+$(".portfolio_item").each(function(i){
+	$(this).find(".popup_content").attr("href", "#work_" + i);
+	$(this).find(".portfolio_descr").attr("id", "work_" + i);
+});
+//
+//---- Portfolio <- end -|
+
+//--Navigation menu links
+$(".drawer-list a").click(function() {
+  $(".click-close").click();
+});
+
+	$(".drawer-list, .menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+
+		//анимируем переход на расстояние - top за 500 мс
+		$('body,html').animate({scrollTop: top}, 500);
+	});
+
+//--Headroom
+$("#header").headroom({
+	"offset": 5,//450
+	"tolerance": 5,
+  "classes": {
+    "initial": "animated",
+    "pinned": "slideDown",
+    "unpinned": "slideUp"
+  }
+});
+
+//--Background change
+// $(window).scroll(function(){
+//     if ($(window).scrollTop() > 480 ){
+//         $(".header").css("background", "#1179bf");
+//     }else{
+//     	$(".header").css("background", "#ffffff");
+//     }
+// });
+
+//-Parallax--start->
+// $(window).scroll(function(e){
+//   parallax();
+// });
+
+// function parallax(){
+//   var scrolled = $(window).scrollTop();
+//   $('.background').css('top',-(scrolled*0.15)+'px');
+// }
+//-Parallax--end-|
+
+//--jqBootstrapValidation
+// $("input, select, textarea").jqBootstrapValidation();
+
+//E-mail Ajax Send
+$("#form_contacts").submit(function() {
+  $(".contacts-submit").addClass("loading");
+  var msg = $(this);
+  $.ajax({
+    type: "POST",
+    url: "../mail.php",
+    data: msg.serialize()
+  }).done(function() {
+    // window.location.href = "#contacts";
+    	$(msg).find('.success').addClass('active').css('display', 'block').hide().fadeIn();
+    // alert("Сообщение отправлено!");
+    setTimeout(function() {
+      // Done Functions
+      $(msg).find('.success').removeClass('active').fadeOut();
+      msg.trigger("reset");
+      $(".contacts-submit").removeClass("loading");
+    }, 2000);
+  });
+  return false;
+});
+
+//--Chrome Smooth Scroll
+	// try {
+	// 	$.browserSelector();
+	// 	if($("html").hasClass("chrome")) {
+	// 		$.smoothScroll();
+	// 	}
+	// } catch(err) {
+	// }
+
+/* Scroll to effect Animate.css -> start */
+
+(function(e){e.fn.visible=function(t,n,r){var i=e(this).eq(0),s=i.get(0),o=e(window),u=o.scrollTop(),a=u+o.height(),f=o.scrollLeft(),l=f+o.width(),c=i.offset().top,h=c+i.height(),p=i.offset().left,d=p+i.width(),v=t===true?h:c,m=t===true?c:h,g=t===true?d:p,y=t===true?p:d,b=n===true?s.offsetWidth*s.offsetHeight:true,r=r?r:"both";if(r==="both")return!!b&&m<=a&&v>=u&&y<=l&&g>=f;else if(r==="vertical")return!!b&&m<=a&&v>=u;else if(r==="horizontal")return!!b&&y<=l&&g>=f}})(jQuery)
+
+		jQuery(window).scroll(function(event) {
+
+		  jQuery(".animate").each(function(i, el) {
+			var el = jQuery(el);
+			if (el.visible(true)) {
+			  el.addClass("start");
+			}
+		  });
+
+		});
+/* Scroll to effect Animate.css -> end */
+
+});
--- src/plugins/animation/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-   var two = '2';
-   var three = '3';
-
-
-    
-
-});
